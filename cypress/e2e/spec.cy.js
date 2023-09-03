b23ac77512eac72ea0bd2913a84a665dfe3caf83
Sandro <sandrobs@outlook.com>
2023-09-03 10:24:31 -0300

    fix cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:5025/')
+    cy.visit('http://localhost:5020/')
   })
 
   /* ==== Test Created with Cypress Studio ==== */
   it('ValidaNavegação', function() {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:5025/');
+    cy.visit('http://localhost:5020/');
     cy.get('label').should('have.text', 'CPF:');
     cy.get(':nth-child(2) > .nav-link').click();
     cy.get('h1').should('have.text', 'Counter');
