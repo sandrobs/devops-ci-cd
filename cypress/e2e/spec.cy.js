bbb74ab3eb17f2aa6d3d660d1479b60f73f452cc
Sandro <sandrobs@outlook.com>
2023-09-03 00:30:36 -0300

    Correções e testes E2E com Cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,18 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5025/')
+  })
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('ValidaNavegação', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:5025/');
+    cy.get('label').should('have.text', 'CPF:');
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get('h1').should('have.text', 'Counter');
+    cy.get('.btn').click();
+    cy.get(':nth-child(3) > .nav-link').click();
+    cy.get('h1').should('have.text', 'Weather forecast');
+    /* ==== End Cypress Studio ==== */
+  });
+})
